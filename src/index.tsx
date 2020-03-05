561059dc9e70e6b1b071ec41ac881eaca1fb3888
Dung <nndungcse@gmail.com>
2020-03-05 09:25:14 +0700

    Input, button , card is customized.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {combineReducers, applyMiddleware, compose, DeepPartial} from 'redux';
-import './index.css';
+import './index.scss';
 import * as serviceWorker from './serviceWorker';
 import { IStoreState } from './types';
 import * as reducers from './reducers';
--- src/scenes/enthusiasm/components/ExampleUIMaterial.tsx
+++ src/scenes/enthusiasm/components/ExampleUIMaterial.tsx
@@ -2,32 +2,43 @@ import * as React from 'react';
 import './ExampleUIMaterial.scss';
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+import { WithStyles, withStyles } from '@material-ui/core';
+import { styles } from '../../../assets/styles/common-styles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
-
-export interface Props {
-    // name: string;
+interface Props extends WithStyles<typeof styles> {
+    children?: React.ReactNode;
+    className?: string;
 }
 
 class ExampleUIMaterial extends React.Component<Props> {
 
-    // constructor(props: Props) {
-    //     super(props);
-
-    // }
-
     render() {
+        const { classes } = this.props;
+        console.log(this.props);
 
         return (
             <div className='example'>
                 <div className="button-demo">
                     <h4>Input Component Demo</h4>
-                    <Button variant="contained">Default</Button>
+                    {/* Override global */}
+                    <Button variant="contained" className="Usonia__Button">Default</Button>
+                    {/* Override separate */}
+                    <Button variant="contained" className={classes.buttonsStyle}>Default</Button>
+
+                    {/* Default button */}
                     <Button variant="contained" color="primary">Primary</Button>
                     <Button variant="contained" color="secondary">Secondary</Button>
                     <Button variant="contained" disabled>Disabled</Button>
                     <Button variant="contained" color="primary" href="#contained-buttons">Link </Button>
                 </div>
                 <div>
+                    <h4>Checkbox</h4>
                     <Checkbox
                         value="primary"
                         inputProps={{ 'aria-label': 'primary checkbox' }}
@@ -65,9 +76,37 @@ class ExampleUIMaterial extends React.Component<Props> {
                         inputProps={{ 'aria-label': 'checkbox with small size' }}
                     />
                 </div>
+                <div>
+                    <h4>Text Input</h4>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <Grid justify="center">
+                                <Grid >
+                                    <TextField id="standard-basic" label="Username" />
+                                </Grid>
+                                <Grid>
+                                    <TextField id="standard-basic" label="Password" />
+                                </Grid>
+                            </Grid>
+                        </Grid>
+
+                    </Grid>
+                </div>
+                <div>
+                    <h4>Card</h4>
+                    <Card>
+                        <CardContent>
+                            <Typography color="textSecondary" gutterBottom>Title</Typography>
+                            <Typography>Content</Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">Learn More</Button>
+                        </CardActions>
+                    </Card>
+                </div>
             </div>
         );
     }
 }
 
-export default ExampleUIMaterial;
+export default withStyles(styles)(ExampleUIMaterial);
